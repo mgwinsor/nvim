42f0e76e6bde5deb603877b5badbcf764113b348
Michael Winsor <mgiwinsor@gmail.com>
2023-01-22 13:46:09 +0800

    fix: Telescope transparency

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,7 @@
 local builtin = require('telescope.builtin')
+vim.cmd 'highlight TelescopeBorder guibg=none'
+vim.cmd 'highlight TelescopeTitle guibg=none'
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
