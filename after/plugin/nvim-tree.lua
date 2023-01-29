98b5a22e6afbf3964cd295f7a5d38a40f993ccc5
Michael Winsor <mgiwinsor@gmail.com>
2023-01-29 22:43:22 +0800

    update: re-enable netrw

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,7 +1,3 @@
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 vim.keymap.set("n", "<leader>d", vim.cmd.NvimTreeToggle)
 
 require("nvim-tree").setup({
