3cf85160780d839df7b0df888c7c4be33d5ae66a
Michael Winsor <mgwinsor@gmail.com>
2023-02-19 13:07:53 +0800

    add: gitsigns pkg and config

--- lua/bhu/packer.lua
+++ lua/bhu/packer.lua
@@ -49,4 +49,8 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    use {
+        'lewis6991/gitsigns.nvim',
+        tag = 'release',
+    }
 end)
